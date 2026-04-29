71008428babf2d77b6d07181b75c560d8c77db88
aman072-hub <amankhuda07@gmail.com>
2026-04-29 19:08:07 +0530

    api changes

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
-app.use("/", authRoutes);
-app.use("/", protectedRoute);
-app.use("/", jobRoutes);
-app.use("/", applicationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/protected", protectedRoute);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/applications", applicationRoutes);
 app.listen(process.env.PORT, () => {
   console.log("Server Running On Port 4000");
 });
